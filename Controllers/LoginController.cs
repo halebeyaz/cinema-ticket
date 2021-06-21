245e10c2b5340ef936a117810317940fe61720bf
halebeyaz <halebeyaz@ceng.deu.edu.tr>
2021-06-21 18:49:27 +0300

    second commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web_cinema.Models;
+
+namespace web_cinema.Controllers
+{
+    public class LoginController : Controller
+    {
+        cinemaEntities db = new cinemaEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            var model = db.admin.ToList();
+            return View();
+        }
+        public bool isLogin(string username, string pass)
+        {
+            admin ad = db.admin.Where(x => x.admin_name.Equals(username) && x.admin_password.Equals(pass)).FirstOrDefault();
+            if (ad != null)
+            {
+                return true;
+            }
+            return new bool();
+            
+        }
+       }
+}
